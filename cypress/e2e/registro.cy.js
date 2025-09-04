2ca344f5d37ce53b4a17ce963b674182b8916724
adweb7 <agusdonalisio@gmail.com>
2025-09-04 12:26:10 -0300

    Dos archivos creados en e2e

--- cypress/e2e/registro.cy.js
+++ cypress/e2e/registro.cy.js
@@ -17,9 +17,9 @@ describe('Registro de Usuario en Ticketazo', () => {
     cy.contains('li', 'Córdoba').click() 
     cy.get('[data-cy="select-localidad"]').click().type('Cordoba');
     cy.contains('.cursor-pointer', 'Córdoba').click();
-    cy.contains('dd').type('07', { delay: 0 })
-    cy.contains('mm').type('12', { delay: 0 })
-    cy.contains('aaaa').type('1995', { delay: 0 })
+    cy.contains('dd').type('07')
+    cy.contains('mm').type('12')
+    cy.contains('aaaa').type('1995')
     cy.get('input[name="email"]').type(randomEmail) // Email único
     cy.get('input[name="confirmarEmail"]').type(randomEmail)
     cy.get('input[name="password"]').type('ContraseñaSegura123')
@@ -44,28 +44,16 @@ describe('Registro de Usuario en Ticketazo', () => {
     //cy.get('button[type="submit"]').click()
   })
 
-  it('No debe permitir registrar con un DNI ya existente', () => {
-    cy.get('input[name="nombres"]').type('Tester')
-    cy.get('input[name="apellido"]').type('Duplicado')
-    cy.get('input[name="telefono"]').type('1234567890')
-    cy.get('input[name="dni"]').type('12345678') // DNI ya registrado
-    cy.get('[data-cy="select-provincia"]').click()   
-    cy.contains('li', 'Córdoba').click() 
-    cy.get('[data-cy="select-localidad"]').click().type('Cordoba');
-    cy.contains('.cursor-pointer', 'Córdoba').click();
-    cy.contains('dd').type('07', { delay: 0 })
-    cy.contains('mm').type('12', { delay: 0 })
-    cy.contains('aaaa').type('1995', { delay: 0 })
-    cy.get('input[name="email"]').type('duplicado@ejemplo.com')
-    cy.get('input[name="confirmarEmail"]').type('duplicado@ejemplo.com')
-    cy.get('input[name="password"]').type('ContraseñaSegura123')
-    cy.get('[data-cy="input-repetir-password"]').type('ContraseñaSegura123')
-    cy.get('button[type="submit"]').click()
-
-    // Verificar que aparece el mensaje de error esperado
-    cy.contains('Ya existe un usuario registrado con ese DNI').should('be.visible')
-  })
-
 
 
 })
+
+
+
+
+
+
+
+
+
+  
---
+++ cypress/e2e/validacion.cy.js
@@ -0,0 +1,29 @@
+describe('Validar que no se repitan DNI y no deje registrar', () => {
+
+  beforeEach(() => {
+    cy.visit('https://ticketazo.com.ar/auth/registerUser')
+  })
+
+  it('No debe permitir registrar con un DNI ya existente', () => {
+    cy.get('input[name="nombres"]').type('Tester')
+    cy.get('input[name="apellido"]').type('Duplicado')
+    cy.get('input[name="telefono"]').type('1234567890')
+    cy.get('input[name="dni"]').type('12345678') // DNI ya registrado
+    cy.get('[data-cy="select-provincia"]').click()   
+    cy.contains('li', 'Córdoba').click() 
+    cy.get('[data-cy="select-localidad"]').click().type('Cordoba');
+    cy.contains('.cursor-pointer', 'Córdoba').click();
+    cy.contains('dd').type('07')
+    cy.contains('mm').type('12')
+    cy.contains('aaaa').type('1995')
+    cy.get('input[name="email"]').type('duplicado@ejemplo.com')
+    cy.get('input[name="confirmarEmail"]').type('duplicado@ejemplo.com')
+    cy.get('input[name="password"]').type('ContraseñaSegura123')
+    cy.get('[data-cy="input-repetir-password"]').type('ContraseñaSegura123')
+    cy.get('button[type="submit"]').click()
+    cy.contains('Ya existe un usuario registrado con ese DNI').should('be.visible')
+  })
+
+
+
+})
